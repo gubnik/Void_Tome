06a95c433dc4699730001427f9cebcf4355e6c1f
nikgub_ <danger8217.nik@gmail.com>
2023-11-24 01:39:35 +0300

    buffed form of nothing

--- src/main/java/net/nikgub/void_tome/enchantments/VTEnchantmentHelper.java
+++ src/main/java/net/nikgub/void_tome/enchantments/VTEnchantmentHelper.java
@@ -61,7 +61,7 @@ public class VTEnchantmentHelper {
         double k = 1.2f;
         source.setDeltaMovement(
                 lx * k,
-                ly * 0.1 * k,
+                ly * 0.25 * k,
                 lz * k
         );
         for(LivingEntity entity : VTUtils.entityCollector(new Vec3(source.getX(), source.getY() + 1.5, source.getZ()), 1, source.level)){
@@ -69,7 +69,7 @@ public class VTEnchantmentHelper {
                 for(VTEnchantmentHelper.Meaning meaning : meanings){
                     meaning.getAttack().accept(source, entity, itemStack);
                 }
-                entity.hurt(VTDamageSource.VOID_NOTHING(source), (float) source.getAttributeValue(VTAttributes.VOID_TOME_DAMAGE.get()) / 2);
+                entity.hurt(VTDamageSource.VOID_NOTHING(source), (float) source.getAttributeValue(VTAttributes.VOID_TOME_DAMAGE.get()) / 1.5f);
             }
         }
     }
